087ea172159273ff2e8d7fcac68619326cae3ea3
Samuel Bacellar <samuelbacellar1@gmail.com>
2022-04-09 16:39:04 -0300

    Criando código do projeto
    Projeto programado em C# (orientação a objetos) que imprime na tela 10 perguntas e entrega ao usuário se a resposta está correta ou não.

---
+++ PerguntaPlus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaEResposta
+{
+    public class PerguntaPlus : Pergunta
+    {
+        public PerguntaPlus() : base()
+        {
+            this.Dica = "";
+        }
+        public PerguntaPlus(String texto, String resposta, String dica) : base(texto, resposta)
+        {
+            this.Dica = dica;
+        }
+
+        public String Dica { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaEResposta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PerguntaPlus p = new PerguntaPlus();
+            p.Texto = "Qual é o site que possui um ótimo conteúdo sobre programação";
+            p.Resposta = "https://www.dfilitto.com.br";
+            p.Dica = "O mantenedor do site é Danilo Filitto";
+
+            Console.WriteLine("Tente adivinhar a pergunta: ");
+            Console.WriteLine("Pergunta: " + p.Texto);
+            Console.WriteLine("Dica: " + p.Dica);
+            Console.Write("Resposta: ");
+            String resposta = Console.ReadLine();
+
+            if (p.Resposta.ToUpper() == resposta.ToUpper())
+            {
+                Console.WriteLine("Parabéns!!! Você Acertou.");
+            }
+            else
+            {
+                Console.WriteLine("Que Pena!!! Você errou.");
+                Console.WriteLine("A resposta correta é: " + p.Resposta);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
